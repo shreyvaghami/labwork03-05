34884997bbc46db3d5ff66168a420e9548bdc8d5
shreyvaghami <131867988+shreyvaghami@users.noreply.github.com>
2023-05-03 15:51:45 +0530

    Add files via upload

---
+++ shrey21.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+ main() 
+     
+     int a=15,b=16,c=17
+     
+     if(a<b)
+{
+	if(a<c)
+	{
+		printf("a is maximun");
+	}
+	else
+	{
+		printf("c is maximum");
+	
+	}
+}
+else
+{
+	if(b<c)
+	{
+		printf("b is maximum");
+	}
+	else
+	    printf("c is maximum")
+}
